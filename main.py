6fdd4f5de3daa338591545cd21ac193912691a01
garawaa <garawaa@gmail.com>
2024-07-11 18:16:49 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,234 @@
+import sys
+import os
+import subprocess
+import threading
+import queue
+from PySide6 import QtWidgets
+from PySide6.QtWidgets import QAbstractItemView, QAbstractScrollArea, QApplication, QHeaderView, QSizePolicy, QTableWidget, QWidget, QFileDialog, QTableWidgetItem, QMenu, QMessageBox
+from PySide6.QtCore import Qt, Signal, Slot
+from ui_main_form import Ui_Form
+
+
+class ResultLine:
+    def __init__(self, line):
+        split = line.split(':')
+        if len(split) > 2:
+            i = 0
+            if len(line) > 1 and line[1] == ':':
+                self.full_path = ':'.join(split[i:i + 2])
+                i += 2
+            else:
+                self.full_path = split[i]
+                i += 1
+            self.full_path = self.full_path.replace('/', '\\')
+            self.line = split[i]
+            i += 1
+            self.contents = split[i]
+            i += 1
+            while i < len(split):
+                self.contents += ':' + split[i]
+                i += 1
+
+            self.file_name = os.path.basename(self.full_path)
+            self.path = os.path.dirname(self.full_path)
+        else:  # Something Weird...
+            self.contents = line
+            self.file_name = ""
+            self.path = ""
+
+    def csv_line(self):
+        return f'"{self.file_name}","{self.path}","{self.line}","{self.contents}"'
+
+
+class MainWidget(QWidget):
+    results_ready = Signal(list)
+
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+
+        self.ui.search_btn.clicked.connect(self.threaded_search)
+        self.ui.reset_btn.clicked.connect(self.reset_fields)
+        self.ui.copy_command_btn.clicked.connect(self.copy_command)
+        self.ui.about_btn.clicked.connect(self.about)
+        self.ui.browse_btn.clicked.connect(self.browse_directory)
+
+        self.ui.result_table.setColumnCount(4)
+        self.ui.result_table.setHorizontalHeaderLabels(
+            ['Path', 'File name', 'Line', 'Contents'])
+        self.ui.result_table.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.ui.result_table.customContextMenuRequested.connect(self.show_context_menu)
+
+        self.ui.result_table.setEditTriggers(
+            QAbstractItemView.EditTrigger.NoEditTriggers)
+        self.ui.result_table.setSelectionBehavior(
+            QAbstractItemView.SelectionBehavior.SelectRows)
+        
+        self.ui.result_table.doubleClicked.connect(self.open_file)
+        self.results_ready.connect(self.update_table)
+        self.result_queue = queue.Queue()
+        
+
+    def browse_directory(self):
+        initial_dir = self.ui.base_path_edit.text()
+        if not os.path.isdir(initial_dir):
+            initial_dir = os.path.expanduser("~")
+        dir_path = QFileDialog.getExistingDirectory(
+            self, "Select Directory", initial_dir)
+        if dir_path:
+            dir_path = dir_path.replace('/', '\\')
+            self.ui.base_path_edit.setText(dir_path)
+
+    def generate_command(self):
+        search_string = self.ui.search_string_edit.text()
+        base_dir = self.ui.base_path_edit.text()
+        file_filter = self.ui.filefilter_edit.text()
+        recursive = self.ui.recursive_checkbox.isChecked()
+        ignore_case = self.ui.ignore_case_checkbox.isChecked()
+        skip_binary = self.ui.skip_binary_checkbox.isChecked()
+        regex = self.ui.regex_checkbox.isChecked()
+
+        if not search_string or not base_dir:
+            QMessageBox.critical(
+                self, "Error", "Please provide all necessary inputs.")
+            return ''
+
+        cmd = ['rg']
+        cmd.append('--line-number')
+        if file_filter:
+            cmd.append('-g')
+            cmd.append(f'{file_filter}')
+        if not recursive:
+            cmd.append('--max-depth=1')
+        if not skip_binary:
+            cmd.append('--binary')
+        if ignore_case:
+            cmd.append('-i')
+        if not regex:
+            cmd.append('--fixed-strings')
+        cmd.append(f'"{search_string}"')
+        cmd.append(f'"{base_dir}"')
+        return ' '.join(cmd)
+
+    def copy_command(self):
+        command = self.generate_command()
+        if command == '':
+            return
+        QApplication.clipboard().setText(command)
+        QMessageBox.information(
+            self, "Copied", f"Command copied to clipboard:\n{command}")
+
+    def search_files(self, command):
+        try:
+            result = subprocess.run(
+                command, capture_output=True, text=True, shell=True, encoding='utf-8'
+            )
+            if result.stdout:
+                self.result_queue.put(result.stdout.splitlines())
+                return
+            if result.stderr:
+                QMessageBox.critical(self, "Error", result.stderr)
+        except Exception as e:
+            QMessageBox.critical(self, "Error", str(e))
+        self.result_queue.put([])
+
+    def update_table(self, results):
+        self.ui.result_table.setRowCount(0)
+        count = 0
+        for index, line in enumerate(results):
+            result_line = ResultLine(line)
+            row_position = self.ui.result_table.rowCount()
+            self.ui.result_table.insertRow(row_position)
+            self.ui.result_table.setItem(
+                row_position, 0, QTableWidgetItem(result_line.path))
+            self.ui.result_table.setItem(
+                row_position, 1, QTableWidgetItem(result_line.file_name))
+            self.ui.result_table.setItem(
+                row_position, 2, QTableWidgetItem(result_line.line))
+            self.ui.result_table.setItem(
+                row_position, 3, QTableWidgetItem(result_line.contents))
+            count += 1
+        # self.ui.result_table.resizeColumnsToContents()
+        self.ui.total_label.setText(f'Total match: {count}')
+        self.ui.progressBar.setRange(0, 100)
+        self.ui.progressBar.setValue(100)
+
+    def threaded_search(self):
+        command = self.generate_command()
+        if command == '':
+            return
+        self.ui.progressBar.setValue(0)
+        self.ui.progressBar.setRange(0, 0)
+        self.ui.total_label.setText("")
+        threading.Thread(target=self.search_thread, args=(command,)).start()
+
+    def search_thread(self, command):
+        self.search_files(command)
+        results = self.result_queue.get()
+        self.results_ready.emit(results)
+
+    def open_file(self):
+        current_row = self.ui.result_table.currentRow()
+        if current_row>-1:
+            path = self.ui.result_table.item(current_row, 0).text()
+            file_name = self.ui.result_table.item(current_row, 1).text()
+            file_path = os.path.join(path, file_name)
+            if os.path.exists(file_path):
+                os.startfile(file_path)
+
+    def show_in_explorer(self):
+        current_row = self.ui.result_table.currentRow()
+        if current_row>-1:
+            path = self.ui.result_table.item(current_row, 0).text()
+            file_name = self.ui.result_table.item(current_row, 1).text()
+            file_path = os.path.join(path, file_name)
+            if os.path.exists(file_path):
+                subprocess.run(
+                    ['explorer', '/select,', file_path.replace('/', '\\')])
+
+    def copy_file_path(self):
+        current_row = self.ui.result_table.currentRow()
+        if current_row>-1:
+            path = self.ui.result_table.item(current_row, 0).text()
+            file_name = self.ui.result_table.item(current_row, 1).text()
+            file_path = os.path.join(path, file_name)
+            QApplication.clipboard().setText(file_path)
+            QMessageBox.information(self, "Copied", f"File path copied to clipboard:\n{file_path}")
+
+
+    def show_context_menu(self, pos):
+        context_menu = QMenu(self)
+        open_action = context_menu.addAction("Open with Default Viewer")
+        explorer_action = context_menu.addAction("Show in Explorer")
+        copy_action = context_menu.addAction("Copy File Path")
+        action = context_menu.exec(self.ui.result_table.mapToGlobal(pos))
+        if action == open_action:
+            self.open_file()
+        elif action == explorer_action:
+            self.show_in_explorer()
+        elif action == copy_action:
+            self.copy_file_path()
+
+    def reset_fields(self):
+        self.ui.search_string_edit.clear()
+        self.ui.filefilter_edit.setText("*.*")
+        self.ui.recursive_checkbox.setChecked(True)
+        self.ui.ignore_case_checkbox.setChecked(True)
+        self.ui.skip_binary_checkbox.setChecked(True)
+        self.ui.regex_checkbox.setChecked(True)
+        self.ui.result_table.setRowCount(0)
+        self.ui.progressBar.setRange(0, 100)
+        self.ui.progressBar.setValue(0)
+        self.ui.total_label.setText("")
+
+    def about(self):
+        QMessageBox.information(
+            self, "About", "Ripgrep GUI\nVersion 1.0\nCreated by Garawaa")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWidget()
+    window.show()
+    sys.exit(app.exec())
---
+++ ui_main_form.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'main_form.ui'
+##
+## Created by: Qt User Interface Compiler version 6.7.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QAbstractItemView, QApplication, QCheckBox, QHBoxLayout,
+    QHeaderView, QLabel, QLineEdit, QProgressBar,
+    QPushButton, QSizePolicy, QSpacerItem, QTableWidget,
+    QTableWidgetItem, QVBoxLayout, QWidget)
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(900, 600)
+        self.verticalLayout = QVBoxLayout(Form)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.search_string_label = QLabel(Form)
+        self.search_string_label.setObjectName(u"search_string_label")
+
+        self.horizontalLayout.addWidget(self.search_string_label)
+
+        self.search_string_edit = QLineEdit(Form)
+        self.search_string_edit.setObjectName(u"search_string_edit")
+
+        self.horizontalLayout.addWidget(self.search_string_edit)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.base_path_label = QLabel(Form)
+        self.base_path_label.setObjectName(u"base_path_label")
+
+        self.horizontalLayout_2.addWidget(self.base_path_label)
+
+        self.base_path_edit = QLineEdit(Form)
+        self.base_path_edit.setObjectName(u"base_path_edit")
+
+        self.horizontalLayout_2.addWidget(self.base_path_edit)
+
+        self.browse_btn = QPushButton(Form)
+        self.browse_btn.setObjectName(u"browse_btn")
+
+        self.horizontalLayout_2.addWidget(self.browse_btn)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+
+        self.horizontalLayout_5 = QHBoxLayout()
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
+        self.filefilter_label = QLabel(Form)
+        self.filefilter_label.setObjectName(u"filefilter_label")
+
+        self.horizontalLayout_5.addWidget(self.filefilter_label)
+
+        self.filefilter_edit = QLineEdit(Form)
+        self.filefilter_edit.setObjectName(u"filefilter_edit")
+
+        self.horizontalLayout_5.addWidget(self.filefilter_edit)
+
+        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout_5.addItem(self.horizontalSpacer_2)
+
+        self.progressBar = QProgressBar(Form)
+        self.progressBar.setObjectName(u"progressBar")
+        self.progressBar.setTextVisible(False)
+
+        self.horizontalLayout_5.addWidget(self.progressBar)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_5)
+
+        self.horizontalLayout_4 = QHBoxLayout()
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.horizontalSpacer_4 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout_4.addItem(self.horizontalSpacer_4)
+
+        self.recursive_checkbox = QCheckBox(Form)
+        self.recursive_checkbox.setObjectName(u"recursive_checkbox")
+        self.recursive_checkbox.setChecked(True)
+
+        self.horizontalLayout_4.addWidget(self.recursive_checkbox)
+
+        self.ignore_case_checkbox = QCheckBox(Form)
+        self.ignore_case_checkbox.setObjectName(u"ignore_case_checkbox")
+        self.ignore_case_checkbox.setChecked(True)
+
+        self.horizontalLayout_4.addWidget(self.ignore_case_checkbox)
+
+        self.skip_binary_checkbox = QCheckBox(Form)
+        self.skip_binary_checkbox.setObjectName(u"skip_binary_checkbox")
+        self.skip_binary_checkbox.setChecked(True)
+
+        self.horizontalLayout_4.addWidget(self.skip_binary_checkbox)
+
+        self.regex_checkbox = QCheckBox(Form)
+        self.regex_checkbox.setObjectName(u"regex_checkbox")
+        self.regex_checkbox.setChecked(True)
+
+        self.horizontalLayout_4.addWidget(self.regex_checkbox)
+
+        self.horizontalSpacer_3 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout_4.addItem(self.horizontalSpacer_3)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_4)
+
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.total_label = QLabel(Form)
+        self.total_label.setObjectName(u"total_label")
+
+        self.horizontalLayout_3.addWidget(self.total_label)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout_3.addItem(self.horizontalSpacer)
+
+        self.search_btn = QPushButton(Form)
+        self.search_btn.setObjectName(u"search_btn")
+
+        self.horizontalLayout_3.addWidget(self.search_btn)
+
+        self.reset_btn = QPushButton(Form)
+        self.reset_btn.setObjectName(u"reset_btn")
+
+        self.horizontalLayout_3.addWidget(self.reset_btn)
+
+        self.copy_command_btn = QPushButton(Form)
+        self.copy_command_btn.setObjectName(u"copy_command_btn")
+
+        self.horizontalLayout_3.addWidget(self.copy_command_btn)
+
+        self.about_btn = QPushButton(Form)
+        self.about_btn.setObjectName(u"about_btn")
+
+        self.horizontalLayout_3.addWidget(self.about_btn)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_3)
+
+        self.result_table = QTableWidget(Form)
+        self.result_table.setObjectName(u"result_table")
+        self.result_table.setContextMenuPolicy(Qt.ContextMenuPolicy.CustomContextMenu)
+        self.result_table.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
+        self.result_table.setSelectionBehavior(QAbstractItemView.SelectionBehavior.SelectRows)
+
+        self.verticalLayout.addWidget(self.result_table)
+
+
+        self.retranslateUi(Form)
+
+        QMetaObject.connectSlotsByName(Form)
+    # setupUi
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"Ripgrep GUI", None))
+        self.search_string_label.setText(QCoreApplication.translate("Form", u"Search string: ", None))
+        self.base_path_label.setText(QCoreApplication.translate("Form", u"Base path: ", None))
+        self.browse_btn.setText(QCoreApplication.translate("Form", u"Browse", None))
+        self.filefilter_label.setText(QCoreApplication.translate("Form", u"File filter:   ", None))
+        self.filefilter_edit.setText(QCoreApplication.translate("Form", u"*.*", None))
+        self.recursive_checkbox.setText(QCoreApplication.translate("Form", u"Recursive", None))
+        self.ignore_case_checkbox.setText(QCoreApplication.translate("Form", u"Ignore case", None))
+        self.skip_binary_checkbox.setText(QCoreApplication.translate("Form", u"Skip binary", None))
+        self.regex_checkbox.setText(QCoreApplication.translate("Form", u"Regex", None))
+        self.total_label.setText(QCoreApplication.translate("Form", u"Total match: ", None))
+        self.search_btn.setText(QCoreApplication.translate("Form", u"Search", None))
+        self.reset_btn.setText(QCoreApplication.translate("Form", u"Reset", None))
+        self.copy_command_btn.setText(QCoreApplication.translate("Form", u"Command to clipboard", None))
+        self.about_btn.setText(QCoreApplication.translate("Form", u"About", None))
+    # retranslateUi
+
